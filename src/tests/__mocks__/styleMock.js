71bdad356d7e060de40b17a3ab0e74257ff06734
Facundo <acosta.facundo.h@gmail.com>
2022-04-23 17:03:51 -0300

    [UP][Task-01] updating README.md file and configuring tests

---
+++ src/tests/__mocks__/styleMock.js
@@ -0,0 +1,19 @@
+/*
+You can do it this way to have an order
+
+const letterA = {...};
+const letterB = {...};
+...
+
+module.exports = {
+  ...letterA,
+  ...letterB,
+
+  ...
+}
+
+*/
+
+module.exports = {
+  description: "description"
+}
